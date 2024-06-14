b699a3ab31dd6af94c713022cb234467e76f8aaf
Vlad Zaiats <vladzaiats7@gmail.com>
2024-06-14 14:41:58 +0300

    new file

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,14 @@
 'use strict';
-const makeTransaction = function (quantity, pricePerDroid) {
-  const result = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${result} credits!`;
+const makeTransaction = function (quantity, pricePerDroid, customerCredits) {
+  const summ = quantity * pricePerDroid;
+  const result =
+    customerCredits >= summ
+      ? `You ordered ${quantity} droids worth ${summ} credits!`
+      : 'Insufficient funds!';
+  return result;
 };
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
