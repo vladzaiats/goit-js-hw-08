c3474f234640336bc6a34ac492ee26978d56ade7
Vlad Zaiats <vladzaiats7@gmail.com>
2024-06-18 17:44:02 +0300

    new file

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,13 @@
 'use strict';
-const getElementWidth = function (content, padding, border) {
-  const a = Number.parseFloat(content);
-  const b = Number.parseFloat(padding);
-  const c = Number.parseFloat(border);
-  const result = a + b * 2 + c * 2;
-  return result;
+const checkForSpam = function (message) {
+  const text = message.toLowerCase();
+  const checkFor = ['spam', 'sale'];
+  return checkFor.some(word => text.includes(word));
 };
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
